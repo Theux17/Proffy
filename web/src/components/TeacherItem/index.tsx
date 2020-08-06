bce7972c7f811738b90cf0a7f3c14f26bfc37e4d
Theux17 <oliveira.ms18@gmail.com>
2020-08-06 00:00:27 -0300

    Finalizada a listagem de usuários

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,34 +2,59 @@ import React from 'react'
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
+import api from '../../services/api'
+
 import './styles.css'
 
-function TeacherItem() {
+export interface Teacher {
+
+    id: number
+    avatar: string
+    bio: string
+    cost: number
+    name: string
+    subject: string
+    whatsapp: string
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection(){
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+    
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/62684121?s=460&u=751b0fcece8eeff4f831387b39fc752cb47c4538&v=4" alt="Matheus Silva" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Matheus Silva</strong>
-                    <span>Química </span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject} </span>
                 </div>
             </header>
             <p>
-                Entusiaste em experiência malucas
-            <br /><br />
-                Apaixonado em explodir Laboratório e consertar vida das pessoas através do método de ensino maluco!
-        </p>
+                {teacher.bio}
+            </p>
 
             <footer>
                 <p>
                     Preço/hora
-                <strong>R$ 50, 00</strong>
+                <strong>R$ {teacher.cost}</strong>
                 </p>
 
-                <button type="button">
-                    <img src={whatsappIcon} alt="Whatsapp" />
-                    Entrar em contato
-            </button>
+
+            <a 
+                target=" blank" 
+                onClick={ createNewConnection } 
+                href={`"https://wa.me/${ teacher.whatsapp }"`}>
+                <img src={whatsappIcon} alt="Whatsapp" />
+                Entrar em contato
+            </a>
             </footer>
 
         </article>
